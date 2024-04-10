a72679f4a975ce3da5fae147e6fdd73bee5ac6f1
MICAELA RICCARDI <47574710@ort>
2024-04-10 10:39:13 -0300

    solo falta mostrar tareas como completadas y buscar por fecha

--- Gestion_de_tareas_Riccardi-Granovsky/script.js
+++ Gestion_de_tareas_Riccardi-Granovsky/script.js
@@ -7,7 +7,8 @@ function Tarea(enunciado, descripcion, vencimiento)
     this.enunciado = enunciado;
     this.estado = "Pendiente";
     this.descripcion = descripcion;
-    if(vencimiento)
+    let hoy = new Date();
+    if(vencimiento && vencimiento > hoy)
     {
         this.vencimiento = vencimiento;
     }
@@ -19,6 +20,7 @@ function Proyecto(nombre, descripcion)
     {
         this.descripcion = descripcion;
     }
+    this.tareas = [];
 }
 function AgregarProyecto(event)
 {
@@ -27,7 +29,6 @@ function AgregarProyecto(event)
     if(!ExisteProyecto(nuevoProyecto.nombre))
     {
         listaProyectos.push(nuevoProyecto);
-        console.log(nuevoProyecto.nombre);
         mensajeError.innerHTML = "";
         MostrarProyectos();
     }
@@ -49,11 +50,70 @@ function MostrarProyectos()
 {
     divProyectos.innerHTML = "";
     listaProyectos.forEach((proyecto, index) => {
-        divProyectos.innerHTML += `<h3>${proyecto.nombre}</h3>
-        <p>${proyecto.descripcion ? `${proyecto.descripcion}` : ``}</p>`;
+        let proyectoDiv = document.createElement('div');
+        proyectoDiv.setAttribute('id', `proyecto${index}`);
+        proyectoDiv.innerHTML += `<h3>${proyecto.nombre}</h3>
+                                <p>${proyecto.descripcion ? `${proyecto.descripcion}` : ``}</p>
+                                <button id="mostrarTareas" onclick = "MostrarTareas(event, ${index})"> Mostrar tarea </button>
+                                <button id="agregarTareas" onclick="AbrirFormTarea(event, ${index})"> Agregar tarea </button>
+                                <div id="inputTareas${index}"></div>`
+        
+        divProyectos.appendChild(proyectoDiv);
     });
 }
-function AgregarTarea(nomProyecto)
-{
 
+function MostrarTareas(event, indexP)
+{
+    event.preventDefault();
+    let proyectoDiv = document.getElementById(`proyecto${indexP}`)
+    listaProyectos[indexP].tareas.forEach((tarea, indexT) =>
+        {
+            proyectoDiv.innerHTML += `<label class="container" onclick="TerminarTarea(${indexP}, ${indexT})">
+                                            <input type="checkbox" checked="checked">
+                                            <div class="checkmark"></div>
+                                        </label>
+                                        <h4>${tarea.estado == "Terminado" ? `<s>${tarea.enunciado}</s>` : `${tarea.enunciado}`}</h4>
+                                        <h5>${tarea.descripcion} ${tarea.vencimiento ? `- Fecha de vencimiento: ${tarea.vencimiento}` : ``}</h5>`;
+        });
+}
+
+function AbrirFormTarea(event, index)
+{
+    event.preventDefault();
+    let inputTareas = document.getElementById(`inputTareas${index}`)
+    let hoy = new Date();
+    hoy = FormatearFecha(hoy);
+    inputTareas.innerHTML = `<form>
+                                <input type="text" maxlength="150" placeholder="Escribe tu nueva tarea..." id="enunciado${index}">
+                                <br>
+                                <textarea id="descTarea${index}" rows="4" cols="50" placeholder="Describe tu tarea brevemente..."></textarea>
+                                <input type="date" min="${hoy}" id="fecha${index}"> 
+                                <button onclick="AgregarTarea(event, ${index})" id="agregar">Agregar</button>
+                                <h5 id="errores"></h5>
+                            </form>`
+}
+
+function AgregarTarea(event, proyectoIndex)
+{
+    event.preventDefault();
+    let enunciadoInput = document.getElementById(`enunciado${proyectoIndex}`);
+    let descTareaInput = document.getElementById(`descTarea${proyectoIndex}`);
+    let fechaInput = document.getElementById(`fecha${proyectoIndex}`);
+
+    let nuevaTarea = new Tarea(enunciadoInput.value, descTareaInput.value, fechaInput.value);
+    listaProyectos[proyectoIndex].tareas.push(nuevaTarea);
+    MostrarProyectos();
+}
+
+function FormatearFecha(fecha) {
+    let dia = ("0" + fecha.getDate()).slice(-2);
+    let mes = ("0" + (fecha.getMonth() + 1)).slice(-2);
+    let anio = fecha.getFullYear();
+
+    return `${anio}/${mes}/${dia}`;
+}
+
+function TerminarTarea(indexProyecto, indexTarea)
+{
+    listaProyectos[indexProyecto].tareas[indexTarea].estado = "Terminado";
 }
