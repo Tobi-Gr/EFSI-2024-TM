a6af8779eb2869da20eee0530ec29dc2b63318c7
Android Studio <devandroid@ort>
2024-07-31 08:24:05 -0300

    estilo layout

--- Gestor-citas-Next-Riccardi-Granovsky/src/app/who-are-we/page.js
+++ Gestor-citas-Next-Riccardi-Granovsky/src/app/who-are-we/page.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 export default function WhoAreWe() {
   return (
-    <main className={styles.main}>
+    <main className={styles.layout}>
       <h1>Quienes somos</h1>
       <Link href="/">Volver</Link>
     </main>
