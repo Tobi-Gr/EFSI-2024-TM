299104d84f5cf7d630731208ec34822381b31645
Android Studio <devandroid@ort>
2024-07-31 09:06:13 -0300

    Who are we

--- Gestor-citas-Next-Riccardi-Granovsky/src/app/contact/page.js
+++ Gestor-citas-Next-Riccardi-Granovsky/src/app/contact/page.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export default function Contact() {
   return (
-    <main class={styles.layout}>
+    <main class={styles.layoutColumn}>
       <h1 class={styles.anotherTitle} >Contacto</h1>
 
       <ul class={styles.noBullets}>
--- Gestor-citas-Next-Riccardi-Granovsky/src/app/who-are-we/page.js
+++ Gestor-citas-Next-Riccardi-Granovsky/src/app/who-are-we/page.js
@@ -1,16 +1,30 @@
 import styles from "./../page.module.css";
 import WhoAreWe from "../components/WhoAreWe";
-//import ButtonLink from "./../components/Button";
 import Link from 'next/link';
-export default function WhoAreWe() {
+
+export default function WhoAreWePage() {
   return (
-    <main className={styles.layout}>
+    <main className={styles.layoutColumn}>
       <h1>Quienes somos</h1>
-      <WhoAreWe
-        image = "https://recursos.ort.edu.ar/static/archivos/usuarioperfil/80344"
-        alt = "Micaela Riccardi"
-        texto = "Micaela Riccardi"
-      />
+      <br/>
+      <div className={styles.aboutUs}>
+        <div /*className={styles.person}*/>
+          <WhoAreWe
+            image = "https://recursos.ort.edu.ar/static/archivos/usuarioperfil/80344"
+            alt = "Micaela Riccardi"
+            texto = "Micaela Riccardi"
+          />
+        </div>
+        <div /*className={styles.person}*/>
+          <WhoAreWe
+            image = "https://recursos.ort.edu.ar/static/archivos/usuarioperfil/81978"
+            alt = "Tobias Granovsky"
+            texto = "Tobias Granovsky"
+          />
+        </div>
+      </div>
+      
+      <br/>
       <Link href="/">Volver</Link>
     </main>
   );
