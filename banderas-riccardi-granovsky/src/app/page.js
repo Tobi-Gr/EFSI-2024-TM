a0e7c8f90856e466e6c07676fe079ae29212aa6f
Android Studio <devandroid@ort>
2024-08-01 11:59:05 -0300

    función para conseguir las respuestas

--- banderas-riccardi-granovsky/src/app/page.js
+++ banderas-riccardi-granovsky/src/app/page.js
@@ -1,15 +1,17 @@
 "use client";
 //foto para el fondo: https://www.freepik.es/vector-gratis/coleccion-banderas-ondeando_1152871.htm#fromView=search&page=1&position=6&uuid=e074ce47-8139-4f19-8b24-c4020251f31f
 
-
 import { useEffect, useState } from 'react';
 import Image from "next/image";
 import styles from "./page.module.css";
 
+
 export default function Home() {
   const [countries, setCountries] = useState([]);
   const [error, setError] = useState(null);
-  const [usedCountries, setUsedCountries] = useState([]);
+  const [unusedCountries, setUnusedCountries] = useState([]);
+  const [currentCounty, setCurrentCountry] = useState(); // por el que se está preguntando
+  let options = [];
 
   useEffect(() => {
     const fetchCountries = async () => {
@@ -20,6 +22,7 @@ export default function Home() {
         }
         const json_data = await res.json();
         setCountries(json_data.data);
+        setUnusedCountries(json_data.data);
       } catch (error) {
         setError(error.message);
       }
@@ -28,19 +31,50 @@ export default function Home() {
     fetchCountries();
   }, []);
 
-  const getRandom = () => {
-    if (countries.length === 0) return; // Handle the case where the array might be empty
-    let randomIndex = Math.floor(Math.random() * countries.length);
-    let selectedCountry = countries[randomIndex];
-    const updatedCountries = countries.filter((_, index) => index !== randomIndex);
-    setCountries(updatedCountries);
-    return selectedCountry;
+  const getRandomCountry = () => { //Para conseguir del que va a ser la bandera y la respuesta correcta
+    if (countries.length === 0) return null; // Para cuando este vacío
+    let randomIndex = getRandomNumber(unusedCountries.length);
+    let selectedCountry = unusedCountries[randomIndex];
+    setUnusedCountries(unusedCountries.filter((_, index) => index !== randomIndex));
+    setCurrentCountry(selectedCountry);
+    console.log("país seleccionado: ", currentCounty);
   };
+
+  const getRandomNumber = (max) => { //no incluye a max
+    return Math.floor(Math.random() * max);
+  }
+
+  const getOptions = () => { //van a haber tres respuestas incorrectas y una correcta
+    options = [];
+    let correctAnswerPos = getRandomNumber(5); //devuelve un número random desde 0 hasta 5 no inclusive para la posición de la respuesta correcta
+    for (let i = 0; i< correctAnswerPos; i++) {
+      do {
+        var randomIndex = getRandomNumber(countries.length);
+      } while (options.includes(countries[randomIndex]) || countries[randomIndex] == currentCounty);
+      options.push(countries[randomIndex])
+    }
+    options.push(currentCounty);
+    for (let i = correctAnswerPos; i < 4; i++) {
+      do {
+        randomIndex = getRandomNumber(countries.length);
+      } while (options.includes(countries[randomIndex]) || countries[randomIndex] == currentCounty);
+      options.push(countries[randomIndex])
+    }
+    console.log("opciones: ", options);
+    return options;
+  }
 
   return (
     <main className={styles.main}>
-      <button onClick={() => getRandom()}>PRESS ME </button>
-      {/* You can also add some UI elements here to display selectedCountry or error messages */}
+      <button onClick={getRandomCountry}>1st Press me</button>
+      {currentCounty && currentCounty.flag && (
+      <Image
+        src={currentCounty.flag}
+        height={500}
+        width={700}
+      />
+    )}
+      <button onClick={getOptions}>2nd Press me</button>
     </main>
   );
 }
