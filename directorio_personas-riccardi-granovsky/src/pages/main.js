3e7a7858a47f7797bdd3aa207d4a0be15f112844
Android Studio <devandroid@ort>
2024-09-04 10:38:37 -0300

    main

--- directorio_personas-riccardi-granovsky/src/pages/main.js
+++ directorio_personas-riccardi-granovsky/src/pages/main.js
@@ -1,9 +1,19 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import Personas from './../data/Personas.json';
 
 function MainScreen(navigation) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    </View>
+      <main className="main">
+        <div className="titleContainer">
+          <h1>Directorio de personas</h1>
+        </div>
+        <div className="cardsContainer">
+          {Personas.map(persona => (
+              <CardPersona key={persona.id} persona={persona}/>            
+          ))}
+        </div>
+      </main>
   );
 }
+
+export default MainScreen;
