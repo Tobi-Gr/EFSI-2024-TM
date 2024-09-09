20628b9771a8c2834c3649fcf04772aa1e09a846
Android Studio <devandroid@ort>
2024-09-09 08:33:54 -0300

    Navbar y form contacto

--- directorio-personas-riccardi-granovsky2/src/pages/contacto.js
+++ directorio-personas-riccardi-granovsky2/src/pages/contacto.js
@@ -3,6 +3,32 @@ import * as React from 'react';
 function ContactoScreen(navigation) {
   return (
     <div style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <form class="form">
+          <p class="title">Contacto</p>
+          <p class="message">Dejá tus datos y sigamos en contacto ;D </p>
+              <div class="flex">
+                <label>
+                    <input required="" placeholder="" type="text" class="input"/>
+                    <span>Nombre</span>
+                </label>
+                
+                <label>
+                    <input required="" placeholder="" type="text" class="input"/>
+                    <span>Apellido</span>
+                </label>  
+              </div>
+                <label>
+                    <input required="" placeholder="" type="email" class="input"/>
+                    <span>Email</span>
+                </label> 
+                    
+                <label>
+                    <input required="" placeholder="" type="number" class="input"/>
+                    <span>Edad</span>
+                </label>
+                
+          <button class="submit">Enviar</button>
+      </form>
     </div>
   );
 }
