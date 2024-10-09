597704d78ed03e9a0a50eb2f5f4638d7472a6b08
Android Studio <devandroid@ort>
2024-10-09 09:04:32 -0300

    Filtrado

--- catalogo-productos/src/pages/productos.js
+++ catalogo-productos/src/pages/productos.js
@@ -1,20 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import Datos from './../Datos';
 import CardProducto from '../components/CardProducto';
+import Search from '../components/Search';
 
 function ProductosScreen() {
-  const [patos, setPatos] = useState([]); 
-
+  const [patos, setPatos] = useState([]);
+  const [patosFiltrados, setPatosFiltrados] = useState([]);
+  
   useEffect(() => {
-    const datos = Datos(); 
-    setPatos(datos);
+      const datos = Datos(); 
+      setPatos(datos);
+      setPatosFiltrados(datos);
   }, []); 
 
+  const handleSearch = (search) => {
+    if (search) {
+      const results = patos.filter((dato) => 
+        dato.nombre.toLowerCase().includes(search.toLowerCase())
+      );
+      setPatosFiltrados(results);
+    } else {
+      setPatosFiltrados(patos); // Restablece a todos los productos si no hay búsqueda
+    }
+  };
   return (
     <main className="main">
       <h1 className='titulo'>Productos</h1>
+      <div className='busqueda'>
+        <Search onSearch={handleSearch} />
+      </div>
       <div className='productosContainer'>
-        {patos.map((pato, index) => (
+        {patosFiltrados.map((pato, index) => (
           <CardProducto key={index} producto={pato} />
         ))}
       </div>
