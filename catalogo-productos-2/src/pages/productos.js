cb09d0df055b1803090bb95b7ad3ecc4a03fdb4f
Android Studio <devandroid@ort>
2024-10-28 17:12:17 -0300

    Búsqueda por filtro

--- catalogo-productos-2/src/pages/productos.js
+++ catalogo-productos-2/src/pages/productos.js
@@ -1,3 +1,84 @@
+// // import Datos from './../Datos';
+// import React, { useEffect, useState } from 'react';
+// import CardProducto from '../components/CardProducto';
+// import Search from '../components/Search';
+// import { useProducts } from './../productsContext';
+
+// function ProductosScreen() {
+//   const { productsData = [] } = useProducts() || {};
+//   const [error, setError] = useState(null);
+//   const [isLoaded, setIsLoaded] = useState(false);
+//   const [items, setItems] = useState([]);
+//   const [q, setQ] = useState("");
+//   const [filterParam, setFilterParam] = useState("All");
+//   const [productosFiltrados, setProductosFiltrados] = useState([]);
+
+//   useEffect(() => {
+//     fetch("https://dummyjson.com/products")
+//         .then((res) => res.json())
+//         .then(
+//             (result) => {
+//                 setIsLoaded(true);
+//                 setItems(result.products);
+//             },
+//             (error) => {
+//                 setIsLoaded(true);
+//                 setError(error);
+//             }
+//         );
+// }, []);
+
+//   useEffect(() => {
+//     setProductosFiltrados(productsData);
+//   }, [productsData]);
+
+//   const handleSearch = (search) => {
+//     if (search) {
+//       const results = productsData.filter((producto) => 
+//         producto.title.toLowerCase().includes(search.toLowerCase())
+//       );
+//       setProductosFiltrados(results);
+//     } else {
+//       setProductosFiltrados(productsData); // Restablece a todos los productos si no hay búsqueda
+//     }
+//   };
+
+//   return (
+//     <main className="main">
+//       <h1 className='titulo'>Productos</h1>
+//       <div className='busqueda'>
+//         <Search onSearch={handleSearch} />
+//       </div>
+//       <div>
+//       <select
+//         onChange={(e) => {
+//             setFilterParam(e.target.value);
+//         }}
+//         className="custom-select"
+//         aria-label="Filtrar por categoría"
+//       >
+//         <option value="All"></option>
+//         <option value="beauty">Beauty</option>
+//         <option value="fragrances">Fragrances</option>
+//         <option value="furniture">Furniture</option>
+//         <option value="groceries">Groceries</option>
+//        </select>
+//       </div>
+//       <div className='productosContainer'>
+//         {productosFiltrados ? (
+//           productosFiltrados.map((producto, index) => (
+//             <CardProducto key={index} producto={producto} />        
+//           ))
+//         ) : (
+//           <p>No hay resultados para tu búsqueda</p>
+//         )}
+//       </div>
+//     </main>
+//   );
+// }
+
+// export default ProductosScreen;
+// src/pages/ProductosScreen.js
 import React, { useState, useEffect } from "react";
 import CardProducto from '../components/CardProducto'; // Asegúrate de la ruta
 
